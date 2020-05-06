729897085af750a707d74a043fd5c6ac7de94a7a
Boue Melaine <melaine.boue@gmail.com>
2020-05-07 00:08:03 +0200

    initialisation

---
+++ src/main/java/com/melaineboue/gestioninvestissementapi/GestioninvestissementapiApplication.java
@@ -0,0 +1,13 @@
+package com.melaineboue.gestioninvestissementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioninvestissementapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioninvestissementapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melaineboue/gestioninvestissementapi/GestioninvestissementapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melaineboue.gestioninvestissementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioninvestissementapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
